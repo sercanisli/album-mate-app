795165bb71c0b4e60ba8a9f40f2635e6a9034bdb
Sercan ISLI <sercanislii@gmail.com>
2024-02-06 03:11:12 +0300

    Adding a new user has been defined. Faker-js was used for adding users. A spinner was defined for the time elapsed during the addition process. Tags structure was created to reflect the database immediately after the addition and deletion process

--- src/components/UsersList.js
+++ src/components/UsersList.js
@@ -1,11 +1,14 @@
 import React from 'react'
-import { useFetchUsersQuery } from '../store/apis/usersApi'
+import { useFetchUsersQuery, useAddUserMutation } from '../store/apis/usersApi'
 import Skeleton from '@mui/material/Skeleton';
 import UsersListItem from './UsersListItem';
+import Button from '@mui/material/Button';
+import  CircularProgress  from '@mui/material/CircularProgress';
 
 function UsersList() {
 
   const {data, isError, isFetching} = useFetchUsersQuery();
+  const [addUser,result] = useAddUserMutation();
 
   let content;
   if(isFetching){
@@ -28,8 +31,27 @@ function UsersList() {
     })
   }
 
+  const handleUserAdd = ()=>{
+    addUser();
+  }
+
   return (
     <div>
+      <div className='topArrangement'>
+        <h1 style={{fontSize:'20px'}}>Kişiler</h1>
+        <Button 
+          variant='outlined'
+          onClick={handleUserAdd}
+        >
+          {
+            result.isLoading ? (
+              <CircularProgress />
+            ) : (
+              <span>Kişi Ekle</span>
+            )
+          }
+        </Button>
+      </div>
       {
         content
       }
--- src/store/apis/usersApi.js
+++ src/store/apis/usersApi.js
@@ -1,4 +1,5 @@
 import {createApi, fetchBaseQuery} from '@reduxjs/toolkit/query/react';
+import {faker} from '@faker-js/faker';
 
 const pause = (duration) => {
     return new Promise((resolve)=> {
@@ -18,6 +19,7 @@ const usersApi = createApi({
     endpoints(builder) {
         return {
             fetchUsers:builder.query({
+                providesTags:['User'],
                 query:() => {
                     return {
                         url:'/users',
@@ -26,17 +28,23 @@ const usersApi = createApi({
                 }
             }),
             addUser:builder.mutation({
+                invalidatesTags:() => { 
+                    return[{type:'User'}]
+                },
                 query:() => {
                     return {
                         url:'/users',
                         method:'POST',
                         body:{
-                            name:"Can"
+                            name: faker.name.fullName() 
                         }
                     }
                 }
             }),
             removeUser:builder.mutation({
+                invalidatesTags:() => { 
+                    return[{type:'User'}]
+                },
                 query:(user) => {
                     return {
                         url:`'/users/${user.id}`,
